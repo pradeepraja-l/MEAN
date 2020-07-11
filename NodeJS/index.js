489ddd46d9929a5a226b6d5b93ee7cf9a554024c
pradeepraja-l <pradeeprajaei@gmail.com>
2020-07-11 22:14:08 +0530

    Adding angularapp

--- NodeJS/index.js
+++ NodeJS/index.js
@@ -1,11 +1,13 @@
 const express = require('express'); //step 1
 const bodyParser = require('body-parser'); //step 1
+const cors = require('cors');///after angularapp creation
 
 const {mongoose} = require('./db.js');   //importing from db.js // step 1 
 var employeeController = require('./controllers/employeeController.js'); // step 2 //importing
 
 var app = express();
 app.use(bodyParser.json());  //configuring express middleware //step 1
+app.use(cors({origin: 'http://localhost:4200'}));
 
 //starting express server
 app.listen(3000, () => console.log('server started at port 3000')); // step 1
